cf1ca97aa4198430d83ff833ff851e051269eb13
Hoàng A'nh <159562977+DNHA05@users.noreply.github.com>
2026-03-02 18:17:19 +0700

    Update views.js

--- api/views.js
+++ api/views.js
@@ -1,9 +1,22 @@
-let views = 0;
+import fs from "fs";
+
+const filePath = "/tmp/views.txt";
 
 export default function handler(req, res) {
-  views += 1;
+  let views = 0n;
+
+  // Nếu file tồn tại thì đọc
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, "utf8");
+    views = BigInt(data);
+  }
+
+  views = views + 1n;
+
+  // Lưu lại
+  fs.writeFileSync(filePath, views.toString());
 
   res.status(200).json({
-    views: views
+    views: views.toString()
   });
 }
