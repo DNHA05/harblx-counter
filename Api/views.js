d6733754886be13283e97ebbc875256f0c94f97d
Hoàng A'nh <159562977+DNHA05@users.noreply.github.com>
2026-03-02 12:35:40 +0700

    Refactor views handler to return random views

--- Api/views.js
+++ Api/views.js
@@ -1,10 +1,5 @@
-let views = 0;
-
 export default function handler(req, res) {
-  if (req.method === "GET") {
-    views++;
-    res.status(200).json({ views });
-  } else {
-    res.status(405).json({ message: "Method Not Allowed" });
-  }
+  res.status(200).json({
+    views: Math.floor(Math.random() * 1000)
+  });
 }
